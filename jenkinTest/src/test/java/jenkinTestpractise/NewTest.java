0d0b29df3ddd823595123fc7d2838579cd27d323
valmiki.k <valmiki.k@DESKTOP-Q5EI90L.intelgain.in>
2019-09-17 12:10:30 +0530

    Excel code and TestNg Annotation added

--- jenkinTest/src/test/java/jenkinTestpractise/NewTest.java
+++ jenkinTest/src/test/java/jenkinTestpractise/NewTest.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -25,12 +26,18 @@ import org.testng.annotations.Test;
 public class NewTest {
 	WebDriver driver;
 
-	@Parameters("Url")
+	
 	@Test
-	public void f(String Url) throws Exception {
+	public void f() throws Exception {
 
 		// driver.get("http://demo.guru99.com/test/guru99home/");
-		driver.get(Url);
+		Excelcode.configexcel(Excelcode.excelpath, "Sheet1");
+		String Username= Excelcode.Getexceldata(1, 0);
+		String password= Excelcode.Getexceldata(1, 1);
+		driver.findElement(By.id("Email")).sendKeys(Username);
+		driver.findElement(By.id("password")).sendKeys(password);
+		driver.findElement(By.className("sub-mit")).click();
+		Thread.sleep(5000);
 		// String title = driver.getTitle();
 		// Assert.assertTrue(title.contains("Google"));
 		// Excelcode.configexcel(Excelcode.excelpath, "Sheet1");
@@ -40,9 +47,9 @@ public class NewTest {
 
 	}
 
-	@Parameters("browser")
+	@Parameters({"browser","Url"})
 	@BeforeTest
-	public void beforeTest(String browser) {
+	public void beforeTest(String browser,String Url) {
 
 		// WebDriverManager.chromedriver().setup();
 
@@ -57,20 +64,31 @@ public class NewTest {
 			System.setProperty("webdriver.gecko.driver", "D:\\valmiki\\geckodriver.exe");
 			driver = new FirefoxDriver();
 		}
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
+		driver.get(Url);
+	}
+	
+	@AfterTest //we can also use @AfterMethod
+	public void setupclosed()
+	{
+		driver.quit();
 	}
 
 	@AfterMethod
 	public void afterTest(ITestResult result) throws IOException {
-
+		
+		
 		if (ITestResult.FAILURE == result.getStatus()) {
+			System.out.println("Inside takescreenshot method");
 			TakesScreenshot ts = ((TakesScreenshot) driver);
 			File source = ts.getScreenshotAs(OutputType.FILE);
 			File des = new File("D:\\valmiki\\Selenium program New\\jenkinTest\\screenshot\\test3.png");
 			FileUtils.copyFile(source, des);
+			driver.quit();
 		}
-		driver.quit();
+		
 	}
 
 }
